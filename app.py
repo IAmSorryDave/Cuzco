9785662969024e77f3bdf5779b84bff977697c92
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2025-10-13 19:51:26 -0700

    Update app.py
    Please don’t brick

--- app.py
+++ app.py
@@ -1,54 +1,66 @@
 import os
 import subprocess
-import time
 import requests
 
+from collections.abc import Generator
 from litellm import APIConnectionError
 from huggingface_hub import HfApi, InferenceClient
 from my_tools import my_custom_tool
 from smolagents import CodeAgent, GradioUI, InferenceClientModel, LiteLLMModel, CodeAgent
 from time import sleep
 
-MODEL_PROVIDER = os.environ.get("MODEL_PROVIDER", "Qwen")
-MODEL_PARAMS = os.environ.get("MODEL_PARAMS", "3b")
-OLLAMA_MODEL = os.environ.get("OLLAMA_MODEL", f"{MODEL_PROVIDER.lower()}2.5-coder:{MODEL_PARAMS}-instruct")
+LANGUAGE_MODEL_PARAMETERS = os.environ.get("LANGUAGE_MODEL_PARAMETERS", "3b")
+LANGUAGE_MODEL_PROVIDER = os.environ.get("LANGUAGE_MODEL_PROVIDER", "qwen")
+LANGUAGE_MODEL_VERSION = os.environ.get("LANGUAGE_MODEL_VERSION", 2.5)
+OLLAMA_MODEL = os.environ.get(
+    "BASE_LANGUAGE_MODEL", 
+    f"{LANGUAGE_MODEL_PROVIDER.title()}{LANGUAGE_MODEL_VERSION}-coder:{MODEL_PARAMS}-instruct"
+)
 OLLAMA_PORT = int(os.environ.get("OLLAMA_PORT", 11434))
 
-def user_has_hf_inference_credits(token):
+def user_has_hf_inference_credits(token : str) -> bool:
     try:
         api = HfApi(token=token)
         api.whoami()
         client = InferenceClient(token=token)
-        _ = client.text_generation("This is a test.", model=f"{MODEL_PROVIDER}/{OLLAMA_MODEL}", max_new_tokens=1)
+        _ = client.text_generation("This is a test.", model=f"{LANGUAGE_MODEL_PROVIDER}/{OLLAMA_MODEL}", max_new_tokens=1)
         exit_value = True
     except Exception:
         exit_value = False
+    finally:
+        return exit_value
 
-    return exit_value
+def ping_local_ollama_server():
+    return requests.get(f"http://localhost:{OLLAMA_PORT}/api/tags", timeout=2)
 
-def start_ollama() -> bool:
+def start_local_ollama_server() -> bool:
     try:
-        r = requests.get(f"http://localhost:{OLLAMA_PORT}/api/tags", timeout=2)
-        if r.status_code == 200:
+        ping = ping_local_ollama_server()
+        if ping.status_code == 200:
             exit_value = True
     except Exception:
-        pass
-    subprocess.Popen(["ollama", "serve"])
-    for _ in range(10):
-        try:
-            r = requests.get(f"http://localhost:{OLLAMA_PORT}/api/tags", timeout=2)
-            if r.status_code == 200:
-                exit_value = True
-        except Exception:
-            time.sleep(1)
-    return exit_value
+        exit_value = False
+        subprocess.Popen(["ollama", "serve"])
+        for _ in range(10):
+            try:
+                ping = ping_local_ollama_server()
+                if ping.status_code == 200:
+                    exit_value = True
+            except Exception:
+                sleep(1)
+    finally:
 
-def return_local_model_server_connection(retry_interval : int = 10):
+        if exit_value == False:
+            print("Failed to start Ollama.")
+        
+        return exit_value
+
+def return_local_ollama_server_connection(retry_interval : int = 10):
 
         while 2 + 2 != 5:
 
             try:
-                client_connection_to_ollama_server = LiteLLMModel(
+                language_model_client_connection_to_ollama_server = LiteLLMModel(
 
                     model_id=f"ollama_chat/{OLLAMA_MODEL}",
                     api_base=f"http://localhost:{OLLAMA_PORT}",
@@ -65,25 +77,37 @@ def return_local_model_server_connection(retry_interval : int = 10):
 
                 break
 
-        return client_connection_to_ollama_server
+        return language_model_client_connection_to_ollama_server
 
-class AgentSeries:
+class LanguageModelAgentGenerator(Generator):
 
     AgentType = CodeAgent
 
     stream_outputs = True
 
-    planning_interval = 3
+    planning_interval = 2
 
     def __init__(self, *tools):
         self.tools = list(tools)
-        self.ollama_started = False
+        self.__agents_point_to_ollama = False
 
-    def yield_dynamic_inference_service_tool_calling_agent(self):
+    @property
+    def agents_point_to_ollama(self):
+        return self.__agents_point_to_ollama
+        
+    @agents.setter
+    def agents_point_to_ollama(self, new_boolean):
+        if new_boolean:
+            self.__agents_point_to_ollama = new_boolean # Once agents point to Ollama, it should stay that way.
+        
+    def throw(self, type=None, value=None, traceback=None):
+        raise StopIteration
+        
+    def send(self, ignore_me):
 
-        while user_has_hf_inference_credits(os.environ.get("HF_TOKEN")):
+        if user_has_hf_inference_credits(os.environ.get("HF_TOKEN")):
 
-            inference_client = InferenceClientModel(model_id=f"{MODEL_PROVIDER}/{OLLAMA_MODEL}")
+            inference_client = InferenceClientModel(model_id=f"{LANGUAGE_MODEL_PROVIDER}/{OLLAMA_MODEL}")
 
             agent = self.AgentType(
                 model=inference_client,
@@ -92,14 +116,12 @@ class AgentSeries:
                 stream_outputs=self.stream_outputs
             )
 
-            yield agent
-
         else:
 
-            if not self.ollama_started:
-                self.ollama_started = start_ollama()
+            if not self.agents_point_to_ollama:
+                self.agents_point_to_ollama = start_local_ollama_server()
 
-            local_inference_client = return_local_model_server_connection()
+            local_inference_client = return_local_ollama_server_connection()
 
             agent = self.AgentType(
                 model=local_inference_client,
@@ -108,8 +130,7 @@ class AgentSeries:
                 stream_outputs=self.stream_outputs
             )
 
-            yield agent
-
+        return agent
 
 class GradioUIWithBackupInference(GradioUI):
 
@@ -125,6 +146,6 @@ class GradioUIWithBackupInference(GradioUI):
 # Use GradioUI from smolagents for the app interface
 if __name__ == "__main__":
     ui = GradioUI(
-        agent=next(GradioUIWithBackupInference.agent_series.yield_dynamic_inference_service_tool_calling_agent())
+        agent=next(GradioUIWithBackupInference.agent_series)
     )
     ui.launch(share=False, server_name="0.0.0.0", server_port=7860)
