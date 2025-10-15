1f0c529f00d1e72c002749a6ff55a65b96deb4e4
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2025-10-14 19:59:16 -0700

    Added ssshhh up.sh

--- up.sh
+++ up.sh
@@ -3,9 +3,13 @@
 IMAGE_NAME="cuzco"
 PORT=7860
 
+read -p "Enter read-only Hugging Face Token: " kung_foo
+
+export MY_SECRET=kung_foo
+
 # Build the Docker image
 echo "Building Docker image: $IMAGE_NAME..."
-docker build -t $IMAGE_NAME .
+docker build --secret id=my_secret_id, env=MY_SECRET -t $IMAGE_NAME .
 
 # Run the Docker container
 echo "Running Docker container on port $PORT..."
