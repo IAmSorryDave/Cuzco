6de54c19970c708e5269d77f463fe46f3b5ec56a
IAmSorryDave <212933284+IAmSorryDave@users.noreply.github.com>
2025-10-19 09:11:39 -0700

    Change Docker image name and simplify build command
    Updated image name and removed secret handling.

--- up.sh
+++ up.sh
@@ -1,15 +1,11 @@
 # Script to build and run the backup inference server Docker container
 
-IMAGE_NAME="cuzco"
+IMAGE_NAME="cuzco-backup-inference-server"
 PORT=7860
-
-read -s -p "Hugging Face Read Access Token: " TOKEN
-
-export MY_SECRET=TOKEN
 
 # Build the Docker image
 echo "Building Docker image: $IMAGE_NAME..."
-docker build --secret id=env,env=MY_SECRET -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME .
 
 # Run the Docker container
 echo "Running Docker container on port $PORT..."
